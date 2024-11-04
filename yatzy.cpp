abfa318946624fc5020c96d24eaf603d12a6f920
alexanderchasacademy <alexander.arvedson@chasacademy.se>
2024-11-04 14:12:18 +0100

    Added a method for throwing the dice

--- yatzy.cpp
+++ yatzy.cpp
@@ -1,4 +1,18 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+class yatzy
+{
+    public: 
+    int diceThrow()
+    {
+        return 1 + rand() % 6;
+    }
+};
 
 int main()
-{}
+{
+    srand(time(NULL));
+
+}
