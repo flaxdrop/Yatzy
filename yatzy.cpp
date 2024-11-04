e4655a15ce893666fdd486960c19d91075184855
Emanuela Asiedu <emanuela.asiedu@chasacademy.se>
2024-11-04 15:12:24 +0100

    player class

--- yatzy.cpp
+++ yatzy.cpp
@@ -13,6 +13,25 @@ class yatzy
 
 int main()
 {
+    std::string playerName, playerName2;
+    Player players[2];
     srand(time(NULL));
-
+    
+    std::cout<< "Ange namnet av spelare 1: " << std::endl;
+    std::cin >> playerName;
+    std::cout<< "Ange namnet av spelare 2: " << std::endl;
+    std::cin >> playerName2;
+     
 }
+
+class Player
+{
+  public:
+  std::string name;
+  int points;
+  int turn;
+
+};
+
+
+
